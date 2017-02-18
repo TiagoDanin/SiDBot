32b81e76e8539977dea001ce7ba7ff8692c50621
Tiago Danin <TiagoDanin@outlook.com>
2017-02-18 09:39:31 -0300

    Added support - Bot in CLI

--- api/cli.py
+++ api/cli.py
@@ -1,6 +1,14 @@
 def show_terminal(text=None):
 	if text:
-		print(text)
+		print('Bot SAY: ' + text)
 		return True
 	else:
 		return False
+
+def check_debug_in_cli(self):
+	if self.debug:
+		#Clean
+		print('- \t - \t - \t - \t - \t - \t - \t - \t -')
+		print('- \t - \t - \t - \t - \t - \t - \t - \t -')
+		print('- \t - \t - \t - \t - \t - \t - \t - \t -')
+	return
--- utils/methods.py
+++ utils/methods.py
@@ -1,11 +1,14 @@
-from api.cli import show_terminal
+from api.cli import show_terminal, check_debug_in_cli
 import PythonColorize as ct
 #from api.telegram import *
 
 def sendText(self, title=None, text=None):
 	# *Title*
 	# text
+	if not title or not text:
+		return False
 	if self.bot_type == 'cli':
+		check_debug_in_cli(self)
 		show_terminal('{bold}{title}{rest}\n{text}'.format(
 						bold=ct.colors.lg_bold_white,
 						title=title,
@@ -33,6 +36,20 @@ def sendList(self, title=None, text=None, results=None):
 	# *Title*
 	# _text_
 	# for in ... (n=0, n+1)results[1] ... results[2]
+	n = 0
+	list_text = ''
+	for v in results:
+		n += 1
+		list_text += str(n) + '. ' + str(v) + '\n'
+	check_debug_in_cli(self)
+	show_terminal('{bold}{title}{rest}\n{text}\n\n{list}'.format(
+					bold=ct.colors.lg_bold_white,
+					title=title,
+					rest=ct.colors.nocolor,
+					text=text,
+					list=list_text
+						)
+					)
 	return True
 
 def sendLog(title=None, date=None, log=None):
